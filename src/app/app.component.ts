35c9c85fed102bf7f2d318475872b28f21cd1447
Diego Rapoport <diegomrapoport@gmail.com>
2024-04-06 10:41:47 -0300

    base form html form builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'forms';
+export class AppComponent implements OnInit {
+  title = 'forms'
+  form: FormGroup = this.fb.group({})
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
 }
